96e2dffe0282285ee2f3c93641667bdd00bb2ebf
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-09-16 15:52:43 -0700

    revising the behavior of conflicts for unifying features.

--- nlgen/cfg/feature.py
+++ nlgen/cfg/feature.py
@@ -7,7 +7,7 @@ class FeatureSet(object):
         pass
 
 
-def unify_features(l, r, match_required=None):
+def unify_features(l, r, match_required=None, ignore=False):
     """
     generate the union of left and right feature dictionaries., and
     raise an exception if there is a conflict in values.
@@ -17,11 +17,19 @@ def unify_features(l, r, match_required=None):
     if match_required is None, all keys must match.
     if match_required is not None, all keys found in the variable
     will require matching (empty collections are valid)
+
+    if ignore is False, no keys will be ignored.
+    if ignore is True, all keys will be ignored.
+    if ignore is a list, only keys within the list will be ignored.
     """
+    if ignore is True:
+        return {}
     l = l.copy()
     r = r.copy()
     union = {}
     for key in set(l.keys()) | set(r.keys()):
+        if ignore and key in ignore:
+            continue
         require_match = match_required is None or key in match_required
         union[key] = _combine_values(l.get(key), r.get(key),
                                      require_match=require_match)
@@ -29,12 +37,15 @@ def unify_features(l, r, match_required=None):
 
 
 def _combine_values(l_values, r_values, require_match=False):
-    if not l_values:
+    if l_values is None:
         return r_values
-    if not r_values:
+    if r_values is None:
         return l_values
-    if require_match and l_values != r_values:
+    if l_values == r_values:
+        return l_values
+    intersection = l_values & r_values
+    if require_match and len(intersection) == 0:
         raise IncongruentFeature(
             "{0} != {1}, and is a required match.".format(l_values, r_values)
         )
-    return l_values | r_values
+    return intersection
--- nlgen/tests/cfg/test_feature.py
+++ nlgen/tests/cfg/test_feature.py
@@ -25,8 +25,40 @@ def test_incongruent_feature():
         unify_features(left, right)
 
 
-def test_incongruent_feature_ignores_incongruent_field():
+def test_incongruent_feature_includes_incongruent_field():
     left = {"gender": set(["female"]), "person": set([1])}
     right = {"gender": set(["male"]), "person": set([1])}
     result = unify_features(left, right, match_required=set(["person"]))
-    assert result == {"gender": set(["female", "male"]), "person": set([1])}
+    assert result == {"gender": set(), "person": set([1])}
+
+
+def test_ignore():
+    left = {"gender": set(["female"]), "person": set([1])}
+    right = {"gender": set(["male"]), "person": set([1])}
+    result = unify_features(left, right, ignore=True)
+    assert result == {}
+
+
+def test_ignore_selection():
+    left = {"gender": set(["female"]), "person": set([1])}
+    right = {"gender": set(["male"]), "person": set([1])}
+    result = unify_features(left, right, ignore=["gender"])
+    assert result == {"person": set([1])}
+
+
+def test_explicit_emptiness_ensures_no_match():
+    """
+    distinguish between explicit emptiness, and allowing all.
+
+    sometimes, it may be important to distinguish between an explicitly
+    empty feature (which will thus have no match), and a FeatureSet that
+    doesn't care about that particular feature.
+
+    None is used to denote ignoring, and an empty set is used to
+    denote allowing no matches.
+    """
+    with pytest.raises(IncongruentFeature):
+        unify_features({"gender": set()}, {"gender": set(["male"])})
+
+    result = unify_features({"gender": None}, {"gender": set(["male"])})
+    assert result == {"gender": set(["male"])}
